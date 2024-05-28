fd2d1d65844b549890020d410a2e972b287dfdc2
UlaMaz <ulamazuraite@gmail.com>
2024-05-28 19:51:12 +0200

    Ordered language frequency by descending order

--- Launcher.py
+++ Launcher.py
@@ -3,31 +3,33 @@ newname = 'which_languages_query.sql'
 with open(newname, 'r')as newSQLfile:
 	Query519 = newSQLfile.read()
 
-print(Query519)
+#print(Query519)
 
 database_file_name = "../SQL_data/converted_catalogue.db"
 SundayDatabase   =  sqlite3.connect(database_file_name)
 SundayCursor = SundayDatabase.cursor()
 SundayCursor.execute(Query519)
-rows=SundayCursor.fetchall()
+queryresults=SundayCursor.fetchall()
 
-totalrows = len(rows)
-print(totalrows)
+totalrows = len(queryresults)
+#print(totalrows)
 
 languagefreq = []
 #for each row number in the range 0 to 10
 for row_number in range(totalrows):
     # print the row number
-    #print(row_number)
-    row = rows[row_number]
+    print(row_number)
+    row = queryresults[row_number]
     languagefreq.append(row[1])
-print(languagefreq)
+    print(row)
 
 
-new_list = sorted(languagefreq)
-print (new_list)
+twenty_most_cmn_lnggs = (queryresults[0:20])
+print("The 20 most common languages are")
+print(twenty_most_cmn_lnggs)
+
+
 # Aim is to create a dictionary with the 20 most common languages
 language_dictionary = {"en":"English",
 						}
-
 
--- which_languages_query.sql
+++ which_languages_query.sql
@@ -1,3 +1,4 @@
 SELECT DISTINCT language, COUNT (language) AS 'number'
 FROM catalogue_entries
 GROUP BY language
+ORDER BY number DESC
