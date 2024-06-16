758ead4412fd1426a53ea11f8dee21105405497f
UlaMaz <ulamazuraite@gmail.com>
2024-06-16 17:34:45 +0200

    Added a for loop to the launcher script

---
+++ Grammar.py
@@ -0,0 +1,95 @@
+### Iteration oer lists  ###################
+
+
+# list_a = ["A", "quite", "naughty", "cat", "."]
+# list_b = list(range(5, 10))
+
+
+# length_of_a = len(list_a)
+
+# print("The number of items in list_a is")
+# print(length_of_a)
+
+# loop_number = 0
+
+# print("Starting the loop;")
+# for loop_number in range(length_of_a):
+# 	print("Going round the loop")
+# 	item_from_a = list_a[loop_number]
+# 	print(item_from_a)
+# print("Loop is done")
+
+# for item_from_a in list_a:
+# 	print("Going round the second loop.")
+# 	print(item_from_a)
+
+# length_of_b = len(list_b)
+
+# print("Starting the loop")
+# for loop_number in range(length_of_b):
+# 	print("Going round the loop")
+# 	item_from_b = list_b[loop_number]
+# 	print(item_from_b)
+# print("Loop is done")
+
+# for item_from_b in list_b:
+# 	print("Going round the second loop.")
+# 	print(item_from_b)
+
+
+########## String formatting  #######
+
+print("Some text to print to the screen.")
+
+number_of_books = 100
+
+my_string = f"The number of books is {number_of_books}. Isn't that nice."
+#print(my_string)
+
+most_common_title = "Best book title"
+
+my_other_string = f"The most common title in the database is {most_common_title}"
+
+my_longer_string = my_string + "\n" +  my_other_string
+print(my_longer_string)
+pi = 3.141592653589793
+
+#print(f"The number pi = {pi:.3}")
+
+# how many words in there title
+length_of_title = [1, 2, 1, 4, 6, 1, 10]
+mean_length_of_title = sum(length_of_title)/len(length_of_title)
+
+#print(f"The mean length of the title is {mean_length_of_title}")
+#print(f"The mean length of the title is {mean_length_of_title:.4}")
+
+empty = ""
+
+if len(most_common_title) < 100:
+	print("Title isn't too long")
+
+# print a message if the mean_length_of_title is greater than 6
+
+if (mean_length_of_title) > 6:
+	print("Not too bad")
+else:
+	print("Titles are a bit short...")
+
+# change this variable from mean_length_of_title to a few others in the file and see of they evaluate as True or False
+
+if empty:
+	print(f"{empty} evaluated to True")
+else:
+	print(f"{empty} evaluated to False")
+
+if 2 in length_of_title:
+	print("Some titles only have 2 letters")
+else:
+	print("2 letter titles don't exist")
+
+# if statement to check if "book" is in my_other_string
+if "book" in my_other_string:
+	print("I like book")
+else:
+	print("Wher buk :(")
+
--- Launcher.py
+++ Launcher.py
@@ -1,72 +1,60 @@
 import sqlite3
+
+######## Languages in the catalogue_entries database ###############
+
+# read in query
 newname = 'which_languages_query.sql'
 with open(newname, 'r')as newSQLfile:
 	Query519 = newSQLfile.read()
 
-#print(Query519)
-
+# opened a connection to the SQL database
 database_file_name = "../SQL_data_copy/converted_catalogue.db"
 SundayDatabase   =  sqlite3.connect(database_file_name)
 SundayCursor = SundayDatabase.cursor()
 
+# run the query and get results
 SundayCursor.execute(Query519)
 queryresults=SundayCursor.fetchall()
 
-totalrows = len(queryresults)
-#print(totalrows)
 
+# print some resuts
+totalrows = len(queryresults)
 languagefreq = []
 #for each row number in the range 0 to 10
-for row_number in range(totalrows):
+for row_number in range(10):
+#for row_number in range(totalrows):
     # print the row number
     print(row_number)
     row = queryresults[row_number]
     languagefreq.append(row[1])
     print(row)
 
+want_to_print_more = False
 
-twenty_most_cmn_lnggs = (queryresults[0:20])
-print("The 20 most common languages are")
-print(twenty_most_cmn_lnggs)
+if want_to_print_more:
+    twenty_most_cmn_lnggs = (queryresults[0:20])
+    print("The 20 most common languages are")
+    print(twenty_most_cmn_lnggs)
 
 
 # Aim is to create a dictionary with the 20 most common languages
 language_dictionary = {"en":"English",
 						}
 
+###################################################################
+
+###### Creating a table for histogam bins #########################
+
+# reading in instructions for table creation
 table_creator_file_name = 'bin_table.sql'
 with open(table_creator_file_name, 'r') as TuesdaySQL:
     TableCreator = TuesdaySQL.read()
 
+# create the new table
 SundayCursor.execute(TableCreator)
 
 
-#### REMOVE #####
-row_creator_file_name = 'createrow.sql'
-with open (row_creator_file_name, 'r') as TuesdaySQL:
-    RowCreator = TuesdaySQL.read()
-SundayCursor.execute(RowCreator)
-###################
-
-##### CREATE row dynamically ######
-item_no = 0
-start_date = 1970
-end_date = 3000
-
-row_creation = 'INSERT INTO Bin_Data'
-table_titles = '(ItemNo, StartDate, EndDate)'
-table_content = f'VALUES ({item_no}, {start_date}, {end_date})'
-
-super_string = row_creation + " " + table_titles + " " + table_content
-SundayCursor.execute(super_string)
-#############
-print(super_string)
-
-TableReader = "SELECT * FROM Bin_Data"
-SundayCursor.execute(TableReader)
-queryresults = SundayCursor.fetchall()
-print(queryresults)
-
+# Find oldest published book
 oldest_entry_file_name = 'SELECtcreated.sql'
 with open(oldest_entry_file_name, 'r') as TodaysSQL:
     OldestDateCreator = TodaysSQL.read()
@@ -74,7 +62,46 @@ SundayCursor.execute(OldestDateCreator)
 queryresults = SundayCursor.fetchall()
 print(queryresults)
 
+##### CREATE row dynamically ######
+item_no = 0
+start_date = 1970
+end_date = 3000
+
+# Now apply that here
+# Put all the lines below up to and including the 'execute' call inside a for loop
+# it should create a set of rows,
+# each one with different item_no, start_date and end_date
+
+row_creation = 'INSERT INTO Bin_Data'
+table_titles = '(ItemNo, StartDate, EndDate)'
+
+for row_number in range(1970, 3001):
+    start_date = row_number
+    end_date= start_date+1
+    item_no= row_number - 1969
+    table_content = f'VALUES ({item_no}, {start_date}, {end_date})'
+    super_string = row_creation + " " + table_titles + " " + table_content
+    SundayCursor.execute(super_string)
+
+#############
+
+
+# Reading what we put into the histogram bin table
+TableReader = "SELECT * FROM Bin_Data"
+SundayCursor.execute(TableReader)
+queryresults = SundayCursor.fetchall()
+print(queryresults)
+
+
+# Delete the hitogram bin table, so we can start afresh next time
 TableDelete = "DROP TABLE 'Bin_Data'"
 SundayCursor.execute(TableDelete)
 queryresults= SundayCursor.fetchall()
 print(queryresults)
+
+###################################################################
+
+
+
+
+  
